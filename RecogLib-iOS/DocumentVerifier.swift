b9cae7884d3b601e61093aec10efbb1a70e8e0e5
Marek Stana <rstanamarek@gmail.com>
2019-06-27 14:46:03 +0200

    naming convention

--- RecogLib-iOS/DocumentVerifier.swift
+++ RecogLib-iOS/DocumentVerifier.swift
@@ -18,17 +18,17 @@ public class DocumentVerifier {
     }
 
     public func verify(buffer: CMSampleBuffer) -> MatcherResult? {
-        return MatcherResult(result: RecogLib_iOS.verify(cppObject, buffer, 0, 0, Int32(DocumentRole.Idc.rawValue), Int32(Country.Cz.rawValue), Int32(PageCodes.F.rawValue)))
+        return MatcherResult(result: RecogLib_iOS.verify(cppObject, buffer, 0, 0, Int32(DocumentRole.Idc.rawValue), Int32(Country.Cz.rawValue), Int32(PageCode.F.rawValue)))
     }
 
     public func verify(buffer: CMSampleBuffer, displayWidth: Double, displayHeight: Double, frameHeight: Double) -> MatcherResult? {
         let aligments = margins(from: displayWidth, displayHeight: displayHeight, frameHeight: frameHeight);
-        return MatcherResult(result: RecogLib_iOS.verify(cppObject, buffer, aligments.horizontal, aligments.vertical, Int32(DocumentRole.Idc.rawValue), Int32(Country.Cz.rawValue), Int32(PageCodes.F.rawValue)))
+        return MatcherResult(result: RecogLib_iOS.verify(cppObject, buffer, aligments.horizontal, aligments.vertical, Int32(DocumentRole.Idc.rawValue), Int32(Country.Cz.rawValue), Int32(PageCode.F.rawValue)))
     }
 
     private func margins(from displayWidth: Double, displayHeight: Double, frameHeight: Double) -> (horizontal: Float, vertical: Float) {
 
-        let frameWidth = frameHeight * 1.585 // Rozměr občanky je 85,6 x 54, to jest 1.585 ku 1.0
+        let frameWidth = frameHeight * 1.585 // ID size is (85,6 x 54), it means 1.585:1.0
         let framePercentageWidth = (frameWidth * 100) / displayWidth
         let framePercentageHeight = (frameHeight * 100) / displayHeight
 
--- RecogLib-iOS/MatcherResult.swift
+++ RecogLib-iOS/MatcherResult.swift
@@ -8,11 +8,11 @@
 
 import Foundation
 
-public enum DocumentCodes: Int {
+public enum DocumentCode: Int {
     case IDC1, IDC2, DRV, PAS, SK_IDC_2008plus, SK_DRV_2004_08_09, SK_DRV_2013, SK_DRV_2015, SK_PAS_2008_14, SK_IDC_1993, SK_DRV_1993, PL_IDC_2015, DE_IDC_2010, DE_IDC_2001, HR_IDC_2013_15, AT_IdentityCard_2000, AT_IDC_2002, AT_IDC_2005, AT_IDC_2010, AT_PAS_2006, AT_DRV_2006, AT_DRV_2013
 }
 
-public enum PageCodes: Int { case F, B }
+public enum PageCode: Int { case F, B }
 
 public enum Country: Int { case Cz, Sk, At, Hu, Pl, De, Hr }
 
@@ -23,15 +23,15 @@ public enum DocumentState: Int { case NoMatchFound, AlignCard, HoldSteady, Blurr
 public struct MatcherResult {
     public var role: DocumentRole
     public var country: Country
-    public var code: DocumentCodes
-    public var page: PageCodes
+    public var code: DocumentCode
+    public var page: PageCode
     public var state: DocumentState
     
     init?(result: CMatcherResult) {
         guard let role = DocumentRole(rawValue: Int(result.documentRole)),
             let country = Country(rawValue: Int(result.documentCountry)),
-            let code = DocumentCodes(rawValue: Int(result.documentCode)),
-            let page = PageCodes(rawValue: Int(result.documentPage)),
+            let code = DocumentCode(rawValue: Int(result.documentCode)),
+            let page = PageCode(rawValue: Int(result.documentPage)),
             let state = DocumentState(rawValue: Int(result.state)) else {
             return nil
         }
