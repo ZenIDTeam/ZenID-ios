b782e56688ba77564ebc46bb65b84222509991b1
Adam Salih <adam.salih@thefuntasty.com>
2019-06-28 11:11:41 +0200

    Add handy Iterable protocol

--- RecogLib-iOS/MatcherResult.swift
+++ RecogLib-iOS/MatcherResult.swift
@@ -8,17 +8,28 @@
 
 import Foundation
 
-public enum DocumentCode: Int {
+public protocol Iterable: CaseIterable {
+    var next: Self { get }
+}
+
+extension Iterable where Self: RawRepresentable, Self.RawValue == Int {
+    public var next: Self {
+        let allCases = Array(Self.allCases)
+        return allCases[(rawValue + 1) % allCases.count]
+    }
+}
+
+public enum DocumentCode: Int, Iterable {
     case IDC1, IDC2, DRV, PAS, SK_IDC_2008plus, SK_DRV_2004_08_09, SK_DRV_2013, SK_DRV_2015, SK_PAS_2008_14, SK_IDC_1993, SK_DRV_1993, PL_IDC_2015, DE_IDC_2010, DE_IDC_2001, HR_IDC_2013_15, AT_IdentityCard_2000, AT_IDC_2002, AT_IDC_2005, AT_IDC_2010, AT_PAS_2006, AT_DRV_2006, AT_DRV_2013
 }
 
-public enum PageCode: Int { case F, B }
+public enum PageCode: Int, Iterable { case F, B }
 
-public enum Country: Int { case Cz, Sk, At, Hu, Pl, De, Hr }
+public enum Country: Int, Iterable { case Cz, Sk, At, Hu, Pl, De, Hr }
 
-public enum DocumentRole: Int { case Idc, Pas, Drv }
+public enum DocumentRole: Int, Iterable { case Idc, Pas, Drv }
 
-public enum DocumentState: Int { case NoMatchFound, AlignCard, HoldSteady, Blurry, ReflectionPresent, Ok }
+public enum DocumentState: Int, Iterable { case NoMatchFound, AlignCard, HoldSteady, Blurry, ReflectionPresent, Ok }
 
 public struct MatcherResult {
     public var role: DocumentRole
