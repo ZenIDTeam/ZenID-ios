61a839fb235032e8a235562041e21a6a2ae4bb01
Adam Salih <adam.salih@thefuntasty.com>
2019-07-01 17:13:56 +0200

    Separate enums to their own file

---
+++ RecogLib-iOS/DocumentEnums.swift
@@ -0,0 +1,90 @@
+//
+//  DocumentEnums.swift
+//  RecogLib-iOS
+//
+//  Created by Adam Salih on 01/07/2019.
+//  Copyright © 2019 Marek Stana. All rights reserved.
+//
+
+import Foundation
+
+public enum DocumentCode: Int {
+    case IDC1, IDC2, DRV, PAS, SK_IDC_2008plus, SK_DRV_2004_08_09, SK_DRV_2013, SK_DRV_2015, SK_PAS_2008_14, SK_IDC_1993, SK_DRV_1993, PL_IDC_2015, DE_IDC_2010, DE_IDC_2001, HR_IDC_2013_15, AT_IdentityCard_2000, AT_IDC_2002, AT_IDC_2005, AT_IDC_2010, AT_PAS_2006, AT_DRV_2006, AT_DRV_2013
+}
+
+extension DocumentCode: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .IDC1:
+            return "IDC1"
+        case .IDC2:
+            return "IDC2"
+        case .DRV:
+            return "DRV"
+        case .PAS:
+            return "PAS"
+        default:
+            return self.rawValue.description
+        }
+    }
+}
+
+public enum PageCode: Int {
+    case Front, Back
+}
+
+extension PageCode: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .Front:
+            return "F"
+        case .Back:
+            return "B"
+        }
+    }
+}
+
+public enum Country: Int {
+    case Cz, Sk, At, Hu, Pl, De, Hr
+}
+
+public enum DocumentRole: Int {
+    case Idc, Pas, Drv
+}
+
+extension DocumentRole: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .Idc:
+            return "Idc"
+        case .Drv:
+            return "Drv"
+        case .Pas:
+            return "Pas"
+        }
+    }
+}
+
+public enum DocumentState: Int {
+    case NoMatchFound, AlignCard, HoldSteady, Blurry, ReflectionPresent, Ok
+}
+
+extension DocumentState: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .NoMatchFound:
+            return "No match found"
+        case .AlignCard:
+            return "Align card"
+        case .HoldSteady:
+            return "Hold steady"
+        case .Blurry:
+            return "Blurry"
+        case .ReflectionPresent:
+            return "Reflection present"
+        case .Ok:
+            return "Ok"
+        }
+    }
+}
+
--- RecogLib-iOS/MatcherResult.swift
+++ RecogLib-iOS/MatcherResult.swift
@@ -8,29 +8,6 @@
 
 import Foundation
 
-public protocol Iterable: CaseIterable {
-    var next: Self { get }
-}
-
-extension Iterable where Self: RawRepresentable, Self.RawValue == Int {
-    public var next: Self {
-        let allCases = Array(Self.allCases)
-        return allCases[(rawValue + 1) % allCases.count]
-    }
-}
-
-public enum DocumentCode: Int, Iterable {
-    case IDC1, IDC2, DRV, PAS, SK_IDC_2008plus, SK_DRV_2004_08_09, SK_DRV_2013, SK_DRV_2015, SK_PAS_2008_14, SK_IDC_1993, SK_DRV_1993, PL_IDC_2015, DE_IDC_2010, DE_IDC_2001, HR_IDC_2013_15, AT_IdentityCard_2000, AT_IDC_2002, AT_IDC_2005, AT_IDC_2010, AT_PAS_2006, AT_DRV_2006, AT_DRV_2013
-}
-
-public enum PageCode: Int, Iterable { case F, B }
-
-public enum Country: Int, Iterable { case Cz, Sk, At, Hu, Pl, De, Hr }
-
-public enum DocumentRole: Int, Iterable { case Idc, Pas, Drv }
-
-public enum DocumentState: Int, Iterable { case NoMatchFound, AlignCard, HoldSteady, Blurry, ReflectionPresent, Ok }
-
 public struct MatcherResult {
     public var role: DocumentRole
     public var country: Country
@@ -50,65 +27,5 @@ public struct MatcherResult {
         self.code = DocumentCode(rawValue: Int(document.code))
         self.country = country
         self.state = state
-    }
-}
-
-extension DocumentState: CustomStringConvertible {
-    public var description: String {
-        switch self {
-        case .NoMatchFound:
-            return "No match found"
-        case .AlignCard:
-            return "Align card"
-        case .HoldSteady:
-            return "Hold steady"
-        case .Blurry:
-            return "Blurry"
-        case .ReflectionPresent:
-            return "Reflection present"
-        case .Ok:
-            return "Ok"
-        }
-    }
-}
-
-extension DocumentRole: CustomStringConvertible {
-    public var description: String {
-        switch self {
-        case .Idc:
-            return "Idc"
-        case .Drv:
-            return "Drv"
-        case .Pas:
-            return "Pas"
-        }
-    }
-}
-
-extension PageCode: CustomStringConvertible {
-    public var description: String {
-        switch self {
-        case .F:
-            return "F"
-        case .B:
-            return "B"
-        }
-    }
-}
-
-extension DocumentCode: CustomStringConvertible {
-    public var description: String {
-        switch self {
-        case .IDC1:
-            return "IDC1"
-        case .IDC2:
-            return "IDC2"
-        case .DRV:
-            return "DRV"
-        case .PAS:
-            return "PAS"
-        default:
-            return self.rawValue.description
-        }
     }
 }
