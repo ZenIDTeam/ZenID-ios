acc9c267a64436006117c4b486837a0eef138d48
Adam Salih <adam.salih@thefuntasty.com>
2019-07-08 09:54:08 +0200

    Add LFS version of OpenCV

---
+++ opencv2.framework/headers/opencv_modules.hpp
@@ -0,0 +1,27 @@
+/*
+ *      ** File generated automatically, do not modify **
+ *
+ * This file defines the list of modules available in current build configuration
+ *
+ *
+*/
+
+// This definition means that OpenCV is built with enabled non-free code.
+// For example, patented algorithms for non-profit/non-commercial use only.
+#define OPENCV_ENABLE_NONFREE
+
+#define HAVE_OPENCV_CALIB3D
+#define HAVE_OPENCV_CORE
+#define HAVE_OPENCV_DNN
+#define HAVE_OPENCV_FEATURES2D
+#define HAVE_OPENCV_FLANN
+#define HAVE_OPENCV_GAPI
+#define HAVE_OPENCV_HIGHGUI
+#define HAVE_OPENCV_IMGCODECS
+#define HAVE_OPENCV_IMGPROC
+#define HAVE_OPENCV_OBJDETECT
+#define HAVE_OPENCV_STITCHING
+#define HAVE_OPENCV_WORLD
+#define HAVE_OPENCV_XFEATURES2D
+
+
