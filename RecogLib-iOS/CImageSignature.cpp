552176f1391a70ed1e3fbe604e2df41d9aa0e9ac
Libor Polehna <polehna.libor@gmail.com>
2021-07-29 14:05:44 +0400

    Fixed RecogLib project files.

--- RecogLib-iOS/CImageSignature.cpp
+++ RecogLib-iOS/CImageSignature.cpp
@@ -1,4 +1,9 @@
+//
+//  CImageSignature.cpp
+//  RecogLib-iOS
+//
+//  Created by Libor Polehna on 29.07.2021.
+//  Copyright © 2021 Marek Stana. All rights reserved.
+//
 
 #include "CImageSignature.hpp"
-
-
