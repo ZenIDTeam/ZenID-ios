fcbd14af66b23b17d671e63e9e67a60d176a55dd
Libor Polehna <polehna.libor@gmail.com>
2021-09-03 08:41:17 +0400

    Hid hologram button.

--- ZenIDDemo/Controller/ChoiceViewController.swift
+++ ZenIDDemo/Controller/ChoiceViewController.swift
@@ -31,7 +31,7 @@ final class ChoiceViewController: UIViewController {
         passportButton,
         otherDocumentButton,
         documentsFilterButton,
-        hologramButton,
+        //hologramButton,
         //faceButton,
         logsButton
     ]
