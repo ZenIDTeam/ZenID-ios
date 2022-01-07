5a6495201b1cbdc8246c2cb0c33fe1a090594029
Libor Polehna <polehna.libor@gmail.com>
2022-01-07 19:15:56 +0400

    Hide instruction view when face mode is faceliveness.

--- ZenIDDemo/Controller/Camera/CameraViewController.swift
+++ ZenIDDemo/Controller/Camera/CameraViewController.swift
@@ -281,7 +281,7 @@ class CameraViewController: UIViewController {
         } else {
             self.showStaticOverlay = true
             self.showVisualisation = true
-            self.showInstructionView = true
+            self.showInstructionView = faceMode != .faceLiveness
         }
         
         // Control view
