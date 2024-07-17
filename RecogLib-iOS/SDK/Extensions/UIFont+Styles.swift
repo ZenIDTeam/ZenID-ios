998f97e64a51d27ab67bef8f319207c0c2156b3f
Vladimir Belohradsky <vbelohradsky@thetrask.com>
2024-07-17 11:24:40 +0200

    [FTR] Remove Roboto requirement.
    Roboto for visualisation v1 is optional from now.

--- RecogLib-iOS/SDK/Extensions/UIFont+Styles.swift
+++ RecogLib-iOS/SDK/Extensions/UIFont+Styles.swift
@@ -13,27 +13,27 @@ public enum ZenFont {
 
 public extension UIFont {
     
-    static let dtitle = UIFont(name: ZenFont.black, size: 25)!
+    static let dtitle = UIFont(name: ZenFont.black, size: 25) ?? UIFont.systemFont(ofSize: 25, weight: .black)
     
-    static let darkTitle = UIFont(name: ZenFont.black, size: 25)!
+    static let darkTitle = UIFont(name: ZenFont.black, size: 25) ?? UIFont.systemFont(ofSize: 25, weight: .black)
     
-    static let darkDescription = UIFont(name: ZenFont.regular, size: 18)!
+    static let darkDescription = UIFont(name: ZenFont.regular, size: 18) ?? UIFont.systemFont(ofSize: 18, weight: .regular)
     
-    static let buttonFont = UIFont(name: ZenFont.black, size: 14)!
+    static let buttonFont = UIFont(name: ZenFont.black, size: 14) ?? UIFont.systemFont(ofSize: 14, weight: .black)
     
-    static let bigTitle = UIFont(name: ZenFont.black, size: 45)!
+    static let bigTitle = UIFont(name: ZenFont.black, size: 45) ?? UIFont.systemFont(ofSize: 45, weight: .black)
     
-    static let title = UIFont(name: ZenFont.black, size: 30)!
+    static let title = UIFont(name: ZenFont.black, size: 30) ?? UIFont.systemFont(ofSize: 30, weight: .black)
     
-    static let version = UIFont(name: ZenFont.regular, size: 14)!
+    static let version = UIFont(name: ZenFont.regular, size: 14) ?? UIFont.systemFont(ofSize: 14, weight: .regular)
     
-    static let topLabel = UIFont(name: ZenFont.bold, size: 17)!
+    static let topLabel = UIFont(name: ZenFont.bold, size: 17) ?? UIFont.systemFont(ofSize: 17, weight: .bold)
     
-    static let messageLabel = UIFont(name: ZenFont.regular, size: 15)!
+    static let messageLabel = UIFont(name: ZenFont.regular, size: 15) ?? UIFont.systemFont(ofSize: 15, weight: .regular)
     
-    static let pagesCountLabel = UIFont(name: ZenFont.bold, size: 18)!
+    static let pagesCountLabel = UIFont(name: ZenFont.bold, size: 18) ?? UIFont.systemFont(ofSize: 18, weight: .bold)
     
-    static let resultTitle = UIFont(name: ZenFont.regular, size: 14)!
+    static let resultTitle = UIFont(name: ZenFont.regular, size: 14) ?? UIFont.systemFont(ofSize: 14, weight: .regular)
     
-    static let resultValue = UIFont(name: ZenFont.bold, size: 20)!
+    static let resultValue = UIFont(name: ZenFont.bold, size: 20) ?? UIFont.systemFont(ofSize: 20, weight: .bold)
 }
